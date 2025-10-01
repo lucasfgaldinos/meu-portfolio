458aef1b840f2153a97768bac7c64b5453029e58
lucasfgaldinos <lucasfgaldinos@outlook.com>
2025-10-01 17:01:46 -0300

    chore: Configuracao do BiomeJS e lint dos arquivos

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <h1>Hello, world!</h1>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<h1>Hello, world!</h1>
+	</StrictMode>,
+);
