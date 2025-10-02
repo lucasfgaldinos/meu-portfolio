7aa60f91623bcebfadb439c6d16f3dd21310fea8
lucasfgaldinos <lucasfgaldinos@outlook.com>
2025-10-02 15:19:04 -0300

    feat: Melhoria de visualizacao no About section

--- src/containers/about/index.tsx
+++ src/containers/about/index.tsx
@@ -1,22 +1,20 @@
 export function About() {
 	return (
-		<section>
-			<div className="section-container">
-				<div className="max-w-2xl text-sm md:text-2xl">
-					<p className="white-text-shadow">
-						Sou Desenvolvedor Web Full Stack, apaixonado por tecnologia e
-						programação. Depois de anos trabalhando com manutenção elétrica,
-						encontrei na programação uma forma de criar soluções práticas e
-						eficientes.
-					</p>
-					<br />
-					<p className="white-text-shadow">
-						Atualmente, foco em projetos pessoais e freelances, aplicando
-						conhecimentos de front-end e back-end com atenção a código limpo,
-						responsividade e qualidade. Busco oportunidades para evoluir como
-						desenvolvedor e contribuir ativamente em times de tecnologia.
-					</p>
-				</div>
+		<section className="section-container">
+			<div className="max-w-2xl text-sm md:text-xl font-medium text-center mx-auto">
+				<p className="white-text-shadow">
+					Sou Desenvolvedor Web Full Stack, apaixonado por tecnologia e
+					programação. Depois de anos trabalhando com manutenção elétrica,
+					encontrei na programação uma forma de criar soluções práticas e
+					eficientes.
+				</p>
+				<br />
+				<p className="white-text-shadow">
+					Atualmente, foco em projetos pessoais e freelances, aplicando
+					conhecimentos de front-end e back-end com atenção a código limpo,
+					responsividade e qualidade. Busco oportunidades para evoluir como
+					desenvolvedor e contribuir ativamente em times de tecnologia.
+				</p>
 			</div>
 		</section>
 	);
