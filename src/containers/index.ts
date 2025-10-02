78ac92d5bc83c2f53334a61e5cbc62a3438922a2
lucasfgaldinos <lucasfgaldinos@outlook.com>
2025-10-02 15:53:22 -0300

    feat: Adicionar fundo de vidro para o Final section

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,4 +1,5 @@
 export * from "./about";
+export * from "./final";
 export * from "./footer";
 export * from "./hero";
 export * from "./skills";
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,13 +1,15 @@
 import { DarkVeil } from "../../components/DarkVeil";
-import { About, Footer, Hero, Skills } from "../../containers";
+import { About, Final, Footer, Hero, Skills } from "../../containers";
 
 export function Home() {
 	return (
 		<>
 			<DarkVeil speed={1} warpAmount={2.5} />
+
 			<Hero />
 			<About />
 			<Skills />
+			<Final />
 			<Footer />
 		</>
 	);
