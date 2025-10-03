924bb415840b46b16fbcece56db8e3ee32c08a08
lucasfgaldinos <lucasfgaldinos@outlook.com>
2025-10-03 16:03:53 -0300

    feat: Remocao do DarkVeil e embla-carousel, ajustes minimos gerais

--- src/containers/projects/index.tsx
+++ src/containers/projects/index.tsx
@@ -1,11 +1,22 @@
+import Autoplay from "embla-carousel-autoplay";
+import useEmblaCarousel from "embla-carousel-react";
 import { ProjectCard } from "../../components/ProjectCard";
+import { projects } from "../../data/projects";
 
 export function Projects() {
+	const [_emblaRef] = useEmblaCarousel({ align: "end", dragFree: false }, [
+		Autoplay({
+			delay: 2000,
+			playOnInit: true,
+			stopOnMouseEnter: true,
+		}),
+	]);
+
 	return (
-		<section className="section-container grid grid-cols-1 md:grid-cols-3 gap-8">
-			<ProjectCard />
-			<ProjectCard />
-			<ProjectCard />
+		<section className="section-container grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+			{projects.map((p) => (
+				<ProjectCard key={p.title} />
+			))}
 		</section>
 	);
 }
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,11 +1,8 @@
-import { DarkVeil } from "../../components/DarkVeil";
 import { About, Final, Footer, Hero, Projects, Skills } from "../../containers";
 
 export function Home() {
 	return (
 		<>
-			<DarkVeil />
-
 			<Hero />
 			<About />
 			<Skills />
