f2d0cb0c4cfe6e8ce01a99093d4a36945ae492c4
lucasfgaldinos <lucasfgaldinos@outlook.com>
2025-10-03 18:19:00 -0300

    feat: Finalizacao do Hero section

--- src/containers/hero/index.tsx
+++ src/containers/hero/index.tsx
@@ -1,3 +1,4 @@
+import { Rocket, SquareArrowOutUpRight } from "lucide-react";
 import { Button } from "react-scroll";
 import { HighlightButton } from "../../components/HighlightButton";
 
@@ -7,31 +8,54 @@ export function Hero() {
 			<div className="section-container flex flex-col items-center justify-center h-screen">
 				<h1
 					className={
-						"white-text-shadow text-8xl font-light text-light text-center"
+						"white-text-shadow text-5xl md:text-8xl font-light text-light text-center"
 					}
 				>
 					Lucas Galdino
 				</h1>
 				<h2
 					className={
-						"white-text-shadow text-5xl font-light text-light text-center mt-10"
+						"white-text-shadow text-xl md:text-4xl font-light text-light text-center mt-6 md:mt-9"
 					}
 				>
 					Desenvolvedor Web Full Stack
 				</h2>
 
-				<div className="flex items-center gap-7 mt-14">
+				<div className="flex flex-col md:flex-row items-center gap-9 mt-16">
 					<Button to="projects" smooth={true} duration={500}>
-						<HighlightButton>Ver projetos</HighlightButton>
+						<HighlightButton>
+							Projetos de destaque <Rocket size={16} />
+						</HighlightButton>
 					</Button>
 					<a
 						href="/DesenvolvedorFullStack-LucasGaldino.pdf"
 						target="_blank"
 						rel="noopener noreferrer"
 					>
-						<HighlightButton>Ver CV </HighlightButton>
+						<HighlightButton>
+							Ver CV <SquareArrowOutUpRight size={16} />
+						</HighlightButton>
 					</a>
 				</div>
+			</div>
+
+			<div className="absolute left-1/2 top-6 transform -translate-x-1/2 -translate-y-1/2 flex items-center gap-8">
+				<a
+					href="https://github.com/lucasfgaldinos"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="text-light/70 hover:text-light text-xs md:text-base font-medium flex items-center gap-2"
+				>
+					GitHub <SquareArrowOutUpRight size={16} />
+				</a>
+				<a
+					href="https://www.linkedin.com/in/lucas-fernando-galdino-da-silva/"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="text-light/70 hover:text-light text-xs md:text-base font-medium flex items-center gap-2"
+				>
+					LinkedIn <SquareArrowOutUpRight size={16} />
+				</a>
 			</div>
 		</section>
 	);
