06eb0e99b5763c578f32093a3b4e41fa91497f6a
lucasfgaldinos <lucasfgaldinos@outlook.com>
2025-10-04 00:59:10 -0300

    Finalizacao do ProjectCard component

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,3 +1,4 @@
+import { Lock, SquareArrowOutUpRight } from "lucide-react";
 import type { Projects } from "../../types/projects";
 import { HighlightButton } from "../HighlightButton";
 import { SkillCard } from "../SkillCard";
@@ -10,21 +11,21 @@ export function ProjectCard({
 	technologies,
 }: Projects) {
 	return (
-		<div className="max-w-md rounded-2xl flex flex-col bg-bg-card shadow-lg">
-			<div className="w-full relative overflow-hidden">
+		<div className="max-w-md rounded-2xl flex flex-col bg-bg-card hover:shadow-[0_0_12px_#7c3aed] hover:border-indigo-800 border border-transparent transition-all duration-200 ease-in-out">
+			<div className="h-56 w-full relative overflow-hidden">
 				<img
-					className="w-full h-64 object-cover rounded-t-2xl brightness-75"
+					className="w-full h-56 object-cover rounded-t-2xl brightness-75"
 					src={banner}
 					alt={`Banner do projeto.`}
 				/>
 			</div>
-			<div className="p-2 md:p-4 flex flex-col justify-between">
+			<div className="p-2 md:p-4 flex flex-col flex-1">
 				<div>
-					<strong className="text-base md:text-xl">{title}</strong>
+					<strong className="text-base lg:text-lg">{title}</strong>
 
 					<hr className="mt-4 text-neutral" />
 
-					<p className="text-xs md:text-base mt-4">{description}</p>
+					<p className="text-xs md:text-sm mt-4">{description}</p>
 
 					<div className="flex flex-wrap gap-2 mt-4">
 						{technologies.map((t) => (
@@ -33,9 +34,33 @@ export function ProjectCard({
 					</div>
 				</div>
 
-				<div className="flex items-center justify-between">
-					{links.deploy && links.repository ? (
-						<HighlightButton>Acessar</HighlightButton>
+				<div className="flex items-end justify-between mt-8 flex-1">
+					<div>
+						{links.deploy ? (
+							<a href={links.deploy} target="_blank" rel="noopener noreferrer">
+								<HighlightButton>
+									Deploy <SquareArrowOutUpRight size={16} />
+								</HighlightButton>
+							</a>
+						) : undefined}
+					</div>
+
+					{links.repository ? (
+						<a
+							href={links.repository}
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<HighlightButton>
+								Código <SquareArrowOutUpRight size={16} />
+							</HighlightButton>
+						</a>
+					) : undefined}
+
+					{!links.deploy && !links.repository ? (
+						<div className="flex items-center justify-center gap-2 text-light font-medium text-lg border border-light rounded-full w-full h-14">
+							Projeto privado <Lock size={16} />
+						</div>
 					) : undefined}
 				</div>
 			</div>
